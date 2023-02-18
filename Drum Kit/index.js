1c606c8758e022e260474a7b36ae282ce0df8146
Coding2co <96010682+Coding2co@users.noreply.github.com>
2023-02-18 10:19:45 +0530

    intinal files

---
+++ Drum Kit/index.js
@@ -0,0 +1,86 @@
+
+var numberOfDrums=document.querySelectorAll(".drum").length;
+for(var i=0;i<numberOfDrums;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+
+         var targetInnerHtml = this.innerHTML;
+          buttonPress(targetInnerHtml);
+
+          buttonAnimation(targetInnerHtml);
+
+    });
+}
+
+document.addEventListener("keypress",function(event){
+    buttonPress(event.key);
+    buttonAnimation(event.key);
+       
+});
+
+
+function buttonPress(key){
+
+    switch(key){
+        case "w":
+            var audio= new Audio("sounds/tom-1.mp3");
+             audio.play();
+             break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "k":
+            var kick = new Audio("sounds/kick.mp3");
+            kick.play();
+            break;
+        case "l":
+            var snare= new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        default:console.log(key);
+     }
+ 
+     
+/*var audio= new Audio("sounds/tom-1.mp3");
+        audio.play();*/
+
+
+}
+
+
+function buttonAnimation(select){
+
+    var buttonActive=document.querySelector("." + select);
+    buttonActive.classList.add("pressed");
+    setTimeout(function () {
+        buttonActive.classList.remove("pressed");
+    },100);
+
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
